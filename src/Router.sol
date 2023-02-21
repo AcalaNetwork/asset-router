d4734f0e42769b59de265cbad4a2802d091b7343
Bryan Chen <xlchen1291@gmail.com>
2023-02-22 11:38:07 +1300

    wip

---
+++ src/Router.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+struct RoutingInstruction {
+    bytes data;
+}
+
+contract Router {
+    RoutingInstruction instruction;
+
+    function route() public {
+        
+    }
+}
