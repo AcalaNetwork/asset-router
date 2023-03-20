53a19dc905bd4c2c6697635182f35817158ba3eb
Shunji Zhan <shunhahaha@live.com>
2023-03-20 20:33:26 +0800

    basic hardhat integration

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # asset-router
+
+## Dev with Foundry
+- install [foundry](https://book.getfoundry.sh/getting-started/installation#installation) if you haven't.
+
+- install deps
+```
+foundryup
+```
+
+- build
+```
+forge build
+```
+
+- test
+```
+forge test
+```
+
+## Deploy with Hardhat
+- install deps
+```
+yarn
+```
+
+- run deploy script
+```
+yarn deploy --network mandala
+```
+
---
+++ scripts/deploy.ts
@@ -0,0 +1,56 @@
+import { ethers } from 'hardhat';
+
+const gasOverride = {
+  gasPrice: '0x33a70303ea',
+  gasLimit: '0x329b140',
+};
+
+async function main() {
+  const [deployer, user] = await ethers.getSigners();
+  const FeeRegistry = await ethers.getContractFactory('FeeRegistry');
+  const Factory = await ethers.getContractFactory('Factory');
+  const Token = await ethers.getContractFactory('MockToken');
+
+  const token = await Token.deploy('tk1', 'TK', gasOverride);
+  await token.deployed();
+
+  const factory = await Factory.deploy(gasOverride);
+  await factory.deployed();
+
+
+  const fee = await FeeRegistry.deploy([{
+    token: token.address,
+    amount: ethers.utils.parseEther('1'),
+  }], gasOverride);
+
+  await fee.deployed();
+
+  console.log(`token address: ${token.address}`);
+  console.log(`factory address: ${factory.address}`);
+  console.log(`feeRegistry address: ${fee.address}`);
+
+  const xcmRouterAddr = await factory.callStatic.deployXcmRouter(fee.address, { dummy: user.address }, gasOverride);
+  console.log({ xcmRouterAddr });
+
+  const _printBalance = async (msg:string) => {
+    console.log(msg, {
+      deployer: ethers.utils.formatEther(await token.balanceOf(deployer.address)),
+      user: ethers.utils.formatEther(await token.balanceOf(user.address)),
+      router: ethers.utils.formatEther(await token.balanceOf(xcmRouterAddr)),
+    });
+  };
+  await _printBalance('init state');
+
+  await token.transfer(xcmRouterAddr, ethers.utils.parseEther('10'), gasOverride);
+  await _printBalance('after wormhole withdraw to router');
+
+  await factory.deployXcmRouterAndRoute(fee.address, { dummy: user.address }, token.address, gasOverride);
+  await _printBalance('after router withdraw to user');
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
