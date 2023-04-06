6d6f9d9039e8ad9a63a4566c39e450e2aea41047
Shunji Zhan <shunhahaha@live.com>
2023-04-06 13:42:14 +0800

    fix router storage (#6)
    * xcm flow works
    
    * fix router storage
    
    * update
    
    * update

--- test/xcm-route.test.ts
+++ test/xcm-route.test.ts
@@ -14,7 +14,7 @@ type Resolved<T> = T extends Promise<infer U> ? U : T;
 
 const feeAddr = '0x8dA2DebebFE5cCe133a80e7114621192780765BB';
 const usdcAddr = '0xE5BA1e8E6BBbdC8BbC72A58d68E74B13FcD6e4c7';
-const factoryAddr = '0x5f46026e680b28c410978989f2a58459c21bc252';
+const factoryAddr = '0xed9ae45a067cadc843e26d377c9cd8e963b299f1';
 
 describe('XcmRouter', () => {
   // fixed
@@ -132,15 +132,6 @@ describe('XcmRouter', () => {
 
   it('after router xcm to user', async () => {
     console.log('\n-------------------- after router xcm to user --------------------');
-    const fundRouterAmount = ethers.utils.parseEther('0.5');
-    await (await relayer.sendTransaction({
-      to: routerAddr,
-      value: fundRouterAmount,
-    })).wait();
-
-    // router need some ACA to pay for storage
-    expect(await relayer.provider!.getBalance(routerAddr)).to.eq(fundRouterAmount);
-
     const deployAndRoute = factory.connect(relayer).deployXcmRouterAndRoute(
       fee.address,
       xcmInstruction,
