fe0dd3659f5b38ecef505260aab2599f7126b210
Shunji Zhan <shunhahaha@live.com>
2023-07-11 00:53:50 +0800

    polished config shape, added acala router tokens (#25)
    * polished config shape, added acala router tokens
    
    * update
    
    * add WBNB on Acala

--- test/config.test.ts
+++ test/config.test.ts
@@ -13,8 +13,8 @@ import { CONTRACTS, ChainName, tryNativeToHexString } from '@certusone/wormhole-
 
 const getProvider = (networkName: string) => {
   const ethRpc = ({
-    [CHAIN.ACALA]: 'https://eth-rpc-acala.aca-staging.network',
-    [CHAIN.KARURA]: 'https://eth-rpc-karura.aca-staging.network',
+    [CHAIN.ACALA]: 'https://eth-rpc-acala.aca-api.network',
+    [CHAIN.KARURA]: 'https://eth-rpc-karura.aca-api.network',
     [CHAIN.ETH]: 'https://ethereum.publicnode.com',
     [CHAIN.ARB]: 'https://endpoints.omniatech.io/v1/arbitrum/one/public',
     [CHAIN.BSC]: 'https://bsc.publicnode.com',
@@ -28,42 +28,50 @@ const getProvider = (networkName: string) => {
 const getWrappedAddr = async (
   dstNetwork: ROUTER_CHAIN,
   srcNetwork: string,
-  tokenAddr: string,
+  srcTokenAddr: string,
 ) => {
   const dstTokenBridge = CONTRACTS.MAINNET[dstNetwork.toLowerCase() as ChainName].token_bridge;
-  const wormholeChainId = CHAIN_NAME_TO_WORMHOLE_CHAIN_ID[srcNetwork as CHAIN_NAME];
+  const srcWormholeChainId = CHAIN_NAME_TO_WORMHOLE_CHAIN_ID[srcNetwork as CHAIN_NAME];
 
-  if (!dstTokenBridge || !wormholeChainId) {
-    throw new Error('cannot find dstTokenBridge or wormholeChainId!');
+  if (!dstTokenBridge || !srcWormholeChainId) {
+    throw new Error('cannot find dstTokenBridge or srcWormholeChainId!');
   }
 
   const tokenBridge = Bridge__factory.connect(dstTokenBridge, getProvider(dstNetwork));
   return tokenBridge.wrappedAsset(
-    wormholeChainId,
-    Buffer.from(tryNativeToHexString(tokenAddr, wormholeChainId), 'hex'),
+    srcWormholeChainId,
+    Buffer.from(tryNativeToHexString(srcTokenAddr, srcWormholeChainId), 'hex'),
   );
 };
 
 describe('config', () => {
   it('source token and dst token match', async () => {
-    const dstNetwork = CHAIN.KARURA;
-    for (const [tokenName, info] of Object.entries(ROUTER_TOKEN_INFO[dstNetwork])) {
+    for (const [tokenName, info] of Object.entries(ROUTER_TOKEN_INFO)) {
       process.stdout.write(`verifying ${tokenName} ...`);
       const srcToken = MockToken__factory.connect(info.originAddr, getProvider(info.originChain));
 
       const [
         symbol,
         decimals,
-        wrappedAddr,
+        wrappedAddrKarura,
+        wrappedAddrAcala,
       ] = await Promise.all([
         srcToken.symbol(),
         srcToken.decimals(),
-        getWrappedAddr(dstNetwork, info.originChain, info.originAddr),
+        getWrappedAddr(CHAIN.KARURA, info.originChain, info.originAddr),
+        getWrappedAddr(CHAIN.ACALA, info.originChain, info.originAddr),
       ]);
 
       expect(symbol).to.equal(tokenName.toUpperCase());
       expect(decimals).to.equal(info.decimals);
-      expect(wrappedAddr).to.equal(info.addr);
+
+      if (info.karuraAddr) {
+        expect(wrappedAddrKarura).to.equal(info.karuraAddr);
+      }
+
+      if (info.acalaAddr) {
+        expect(wrappedAddrAcala).to.equal(info.acalaAddr);
+      }
       console.log('ok');
     }
   });
