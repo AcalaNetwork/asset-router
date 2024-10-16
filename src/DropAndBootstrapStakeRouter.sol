b5caa9bd573c3e866182fb36ca4225647cf185d5
Shunji Zhan <shunhahaha@live.com>
2024-10-16 17:16:56 +0800

    cleanup

--- src/DropAndBootstrapStakeRouter.sol
+++ src/DropAndBootstrapStakeRouter.sol
@@ -87,11 +87,6 @@ contract DropAndBootstrapStakeRouter is BaseRouter {
         selfdestruct(payable(_instructions.feeReceiver));
     }
 
-    function _destroy() private {
-        emit RouterDestroyed(address(this));
-        selfdestruct(payable(_instructions.feeReceiver));
-    }
-
     function claimShareAndStakeTo(ERC20 token) public {
         // ensure bootstrap has been existed and ended
         address lpToken = IDEX(_instructions.dex).getLiquidityTokenAddress(
